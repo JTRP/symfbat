ffc7b82431605f6c876881f9a6d9049ff2927fff
JTRP <Jurek>
2022-05-18 13:55:21 +0200

    Ajout entité article + ajout page création article + page voir un article

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\NewArticleFormType;
+use App\Repository\ArticleRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+#[Route('/blog', name: 'blog_')]
+class BlogController extends AbstractController
+{
+
+    /**
+     * Contrôleur de la page permettant de céer un nouvel article
+     *
+     * Accès reservé au administrateur
+     */
+    #[Route('/nouvelle-publication/', name: 'new_publication')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function newPublication(Request $request , ArticleRepository $articleRepository, SluggerInterface $sluger) : Response
+    {
+
+        $article = new Article();
+
+        $form = $this->createForm(NewArticleFormType::class, $article);
+
+        $form->handleRequest($request);
+
+        // Si le formulaire est envoyé et sans erreur
+        if ( $form->isSubmitted() && $form->isValid() ) {
+
+            // On termine d'hydrater l'article
+            $article
+                ->setPublicationDate( new \DateTime() )
+                ->setAuthor( $this->getUser() )
+                ->setSlug( $sluger->slug( $article->getTitle() )->lower() )
+            ;
+
+            // Sauvegarde de l'article en BDD via le manager général des entités de ArticleRepository
+            $articleRepository->add( $article, true );
+
+            // Message flash succès
+            $this->addFlash('success', 'Article publié avec succès');
+
+            // Redirection vers la page qui affiche l'article (en envoyant son id et sont slug dans l'url)
+            return $this->redirectToRoute('blog_publication_view', [
+                'id' => $article->getId(),
+                'slug' => $article->getSlug()
+            ]);
+
+        }
+
+        return $this->render('blog/new_publication.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * Contrôleur de la paage permettantr de voir l'article ene détail (via id et slug dans l'url)
+     */
+    #[Route('/publication/{id}/{slug}', name: 'publication_view')]
+    #[ParamConverter('article', options: [ 'mapping' => [ 'id' => 'id', 'slug' => 'slug' ] ] )]
+    public function publicationView( Article $article ) : Response
+    {
+
+        return $this->render('blog/publication_view.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+
+
+}
